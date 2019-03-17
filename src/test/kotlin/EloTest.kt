2542d73ecb8bcde6041b1efab5ed6ec32b77fd12
Marcus Nordberg <nordbergaren93@gmail.com>
2019-03-17 20:27:09 +0100

    Use fixtures for tests

--- src/test/kotlin/EloTest.kt
+++ src/test/kotlin/EloTest.kt
@@ -1,33 +1,16 @@
+import fixtures.*
 import org.assertj.core.api.Java6Assertions.assertThat
 import org.junit.Test
 import pingpongtracker.business.Elo
-import pingpongtracker.domain.Match
-import pingpongtracker.domain.Sport
-import pingpongtracker.domain.Team
 
 class EloTest {
 
     @Test
     fun `test calculation of new elos where better player wins`() {
-        val team1 = Team(
-            id = 1L,
-            playerIds = setOf(elo2400Player.id),
-            elo = 2400
-        )
-        val team2 = Team(
-            id = 2L,
-            playerIds = setOf(elo2000Player.id),
-            elo = 2000
-        )
+        val team1 = elo2400TeamOnePlayer
+        val team2 = elo2000TeamOnePlayer
 
-        val match = Match(
-            id = 10L,
-            sport = Sport.PINGPONG,
-            team1 = 1L,
-            team2 = 2L,
-            scoreT1 = 2,
-            scoreT2 = 0
-        )
+        val match = matchWithT1AsWinner
 
         val expectedEloPair = Elo.NewEloPair(2403, 1997)
         val resultEloPair = Elo.calculateElo(team1, team2, match)
@@ -37,25 +20,10 @@ class EloTest {
 
     @Test
     fun `test calculation of new elos where worse player wins`() {
-        val team1 = Team(
-            id = 1L,
-            playerIds = setOf(elo2400Player.id),
-            elo = 2400
-        )
-        val team2 = Team(
-            id = 2L,
-            playerIds = setOf(elo2000Player.id),
-            elo = 2000
-        )
+        val team1 = elo2400TeamOnePlayer
+        val team2 = elo2000TeamOnePlayer
 
-        val match = Match(
-            id = 10L,
-            sport = Sport.PINGPONG,
-            team1 = 1L,
-            team2 = 2L,
-            scoreT1 = 0,
-            scoreT2 = 2
-        )
+        val match = matchWithT2AsWinner
 
         val expectedEloPair = Elo.NewEloPair(2371, 2029)
         val resultEloPair = Elo.calculateElo(team1, team2, match)
